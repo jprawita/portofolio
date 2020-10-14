0a4e1dfd287a87f2d922d930358ad55f19607c83
Jun Prawita <jprawita@gmail.com>
2020-10-14 12:10:27 +0800

    initial commit

---
+++ precache-manifest.df6fe0f5af61e2ee83958c27164f50d7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5624f0019de3d0e5bddd",
+    "url": "/css/app.9397824d.css"
+  },
+  {
+    "revision": "a85014593095536a8e31",
+    "url": "/css/chunk-vendors.bfa393d1.css"
+  },
+  {
+    "revision": "80489b3e8b5c966255dddc4ff50abecb",
+    "url": "/favicon-jp.ico"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "1904b5f7e3ca2c3190ad5cec7bca5ed3",
+    "url": "/img/undraw_Portfolio_update_re_jqnp.1904b5f7.png"
+  },
+  {
+    "revision": "883cdaadbc79fc6387740a1d3b0fc23c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69153440a8e036679397",
+    "url": "/js/about.d9c1dbcb.js"
+  },
+  {
+    "revision": "5624f0019de3d0e5bddd",
+    "url": "/js/app.a76f8660.js"
+  },
+  {
+    "revision": "a85014593095536a8e31",
+    "url": "/js/chunk-vendors.ac6c2166.js"
+  },
+  {
+    "revision": "7641d0dd161fdd4e6e988fd5e1da8c69",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
